86d9eae11debbaf55d12d1fe50f308c623d47501
AASMadhawa <madhawa.amarasinghe96@gmail.com>
2020-04-17 00:46:52 +0530

    Get method added

---
+++ src/main/java/com/HelthCare/HealthCare/user.java
@@ -0,0 +1,86 @@
+package com.HelthCare.HealthCare;
+
+public class user {
+	
+	public int u_id;
+	public String u_fname;
+	public String u_lname;
+	public int u_age;
+	public String u_address;
+	public String u_sex;
+	public String u_email;
+	public String u_username;
+	public String u_password;
+	public String u_type;
+	public int u_contact;
+	
+	
+	public int getU_id() {
+		return u_id;
+	}
+	public void setU_id(int u_id) {
+		this.u_id = u_id;
+	}
+	public String getU_fname() {
+		return u_fname;
+	}
+	public void setU_fname(String u_fname) {
+		this.u_fname = u_fname;
+	}
+	public String getU_lname() {
+		return u_lname;
+	}
+	public void setU_lname(String u_lname) {
+		this.u_lname = u_lname;
+	}
+	public int getU_age() {
+		return u_age;
+	}
+	public void setU_age(int u_age) {
+		this.u_age = u_age;
+	}
+	public String getU_address() {
+		return u_address;
+	}
+	public void setU_address(String u_address) {
+		this.u_address = u_address;
+	}
+	public String getU_sex() {
+		return u_sex;
+	}
+	public void setU_sex(String u_sex) {
+		this.u_sex = u_sex;
+	}
+	public String getU_email() {
+		return u_email;
+	}
+	public void setU_email(String u_email) {
+		this.u_email = u_email;
+	}
+	public String getU_username() {
+		return u_username;
+	}
+	public void setU_username(String u_username) {
+		this.u_username = u_username;
+	}
+	public String getU_password() {
+		return u_password;
+	}
+	public void setU_password(String u_password) {
+		this.u_password = u_password;
+	}
+	public String getU_type() {
+		return u_type;
+	}
+	public void setU_type(String u_type) {
+		this.u_type = u_type;
+	}
+	public int getU_contact() {
+		return u_contact;
+	}
+	public void setU_contact(int u_contact) {
+		this.u_contact = u_contact;
+	}
+	
+
+}
---
+++ src/main/java/com/HelthCare/HealthCare/userResource.java
@@ -0,0 +1,68 @@
+package com.HelthCare.HealthCare;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/users")
+public class userResource {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<user> getAlluser() {
+		
+		List<user> users = new ArrayList<user>();
+		
+		user u1 = new user();
+		u1.setU_id(0001);
+		u1.setU_fname("Supun");
+		u1.setU_lname("Madhawa");
+		u1.setU_age(24);
+		u1.setU_address("Kuliyapitiya");
+		u1.setU_sex("Male");
+		u1.setU_sex("madhawa@gmail.com");
+		u1.setU_username("Madhawa");
+		u1.setU_password("madhawa123");
+		u1.setU_type("admin");
+		u1.setU_contact(07600000000);
+		
+		user u2 = new user();
+		u2.setU_id(0002);
+		u2.setU_fname("Sahan");
+		u2.setU_lname("Malssri");
+		u2.setU_age(23);
+		u2.setU_address("Kolonnawa");
+		u2.setU_sex("Male");
+		u2.setU_sex("sahan@gmail.com");
+		u2.setU_username("Sahan");
+		u2.setU_password("sahan123");
+		u2.setU_type("patient");
+		u2.setU_contact(07700000000);
+		
+		user u3 = new user();
+		u3.setU_id(0003);
+		u3.setU_fname("Kasun");
+		u3.setU_lname("Thilina");
+		u3.setU_age(24);
+		u3.setU_address("Horana");
+		u3.setU_sex("Male");
+		u3.setU_sex("kasun@gmail.com");
+		u3.setU_username("Kasun");
+		u3.setU_password("kasun123");
+		u3.setU_type("patient");
+		u3.setU_contact(07700000000);
+		
+		users.add(u1);
+		users.add(u2);
+		users.add(u3);
+		
+		return users;
+		
+		
+	}
+
+}
